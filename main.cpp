ddddfe3c59fab83080d862634267a1bb0c60a8e0
Lugsole <Lugsole@users.noreply.github.com>
2019-09-10 15:32:26 -0400

    Change array to vector

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <ctime>
 #include <ratio>
 #include <bitset>
+#include <vector> 
 using namespace std;         // Including the C++ Standard Library.
 using namespace std::chrono; // Include chrono standard library
 
@@ -19,12 +20,10 @@ int main()
   unsigned int max_count;
   /* Read the largest number it will be allowed to test against */
   cin >> max_count;
-  unsigned int *prime = new unsigned int[max_count];
+  vector<unsigned int> Prime_Numbers; 
   /* Start with two prime numbers */
-  prime[0] = 2;
-  prime[1] = 3;
-  /* Tells the position latter to start saving numbers too */
-  unsigned int position = 2;
+  Prime_Numbers.push_back(2);
+  Prime_Numbers.push_back(3);
   /* Set the last number tested */
   unsigned int spot = 4;
   bool log = false;
@@ -39,13 +38,13 @@ int main()
     /* Set the largest number to try the square root  */
     unsigned int max_try = sqrt(spot);
     /* check against all the known prime numbers to see if it is prime or not */
-    for (unsigned int j = 0; j < position && max_try >= prime[j]; j++)
+    for (unsigned int j = 0; j < Prime_Numbers.size() && max_try >= Prime_Numbers[j]; j++)
     {
       /* if we are logging then print what index we are testing against */
       if (log)
         cout << "letter j:" << j << endl;
       /* Get the number to test against */
-      unsigned int i = prime[j];
+      unsigned int i = Prime_Numbers[j];
       unsigned int n = spot;
       /* see if not evenly divisible */
       if (n % i == 0)
@@ -61,9 +60,7 @@ int main()
       /* log as prime */
       cout << "Found prime: " << spot << endl;
       /* add to list of prime */
-      prime[position] = spot;
-      /* increase data save counter */
-      position++;
+      Prime_Numbers.push_back(spot);
     }
     /* if not prime */
     else
@@ -92,18 +89,18 @@ int main()
   Uint_File.open("example.bin");
   cout << "List of prime numbers: " << endl;
   /* for all of the numbers print to file */
-  for (unsigned int j = 0; j < position; j++)
+  for (unsigned int j = 0; j < Prime_Numbers.size(); j++)
   {
-    cout << "prime:" << prime[j] << endl;
+    cout << "prime:" << Prime_Numbers[j] << endl;
     /* Prints to the file */
-    myfile << prime[j] << endl;
-    Uint_File << (uint8_t)((prime[j])>>24);
-    Uint_File << (uint8_t)((prime[j])>>16);
-    Uint_File << (uint8_t)((prime[j])>>8);
-    Uint_File << (uint8_t)(prime[j]);
+    myfile << Prime_Numbers[j] << endl;
+    Uint_File << (uint8_t)((Prime_Numbers[j])>>24);
+    Uint_File << (uint8_t)((Prime_Numbers[j])>>16);
+    Uint_File << (uint8_t)((Prime_Numbers[j])>>8);
+    Uint_File << (uint8_t)(Prime_Numbers[j]);
   }
   /* prints the number of prime numbers to the file */
-  myfile << "Found " << position << " primes" << endl;
+  myfile << "Found " << Prime_Numbers.size() << " primes" << endl;
   /* prints amount of time consumed to the file */
   myfile << "Total time consumed: " << time_span.count() << " ms" << endl;
   /* close the file and save the file */
